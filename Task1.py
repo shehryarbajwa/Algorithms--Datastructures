0beb070ce9c3ac4d879149e21ab7df65a4011d42
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-06-26 00:51:27 +0500

    Task1.py incomplete

--- Task1.py
+++ Task1.py
@@ -3,11 +3,11 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
-with open('texts.csv', 'r') as f:
+with open('/Users/shehryarbajwa/algorithms-challenges/texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open('/Users/shehryarbajwa/algorithms-challenges/calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -18,3 +18,23 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+count = 0
+count1 = 0
+i = 0
+j = 1
+
+
+    
+
+while i < len(calls) - 1:
+    if (calls[i][0] != calls[i+1][0]):
+        count = count + 1
+    i = i + 1
+
+    if(calls[j][0] != calls[j+1][0]):
+        count1 = count1 + 1
+    j = j + 1
+
+print("There are " + str(count) + ' different telephone numbers in the records.')
+print(count1)
