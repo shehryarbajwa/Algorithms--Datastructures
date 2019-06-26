8efe97cd1e6e7f8ce883a36a31fa23e858f833da
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-06-27 00:03:09 +0500

    Task1 finished with run time analysis documented

--- Task1.py
+++ Task1.py
@@ -19,22 +19,41 @@ Print a message:
 "There are <count> different telephone numbers in the records."
 """
 
-count = 0
-count1 = 0
-i = 0
-j = 1
+different_list = []
+
+for item in calls[0][0]:
+    if item not in different_list:
+        different_list.append(item)
 
 
-    
+for item in calls[0][1]:
+    if item not in different_list:
+        different_list.append(item)
 
-while i < len(calls) - 1:
-    if (calls[i][0] != calls[i+1][0]):
-        count = count + 1
-    i = i + 1
 
-    if(calls[j][0] != calls[j+1][0]):
-        count1 = count1 + 1
-    j = j + 1
+for item in texts[0][0]:
+    if item not in different_list:
+        different_list.append(item)
+
+for item in texts[0][1]:
+    if item not in different_list:
+        different_list.append(item)
+        count = len(different_list)
+
 
 print("There are " + str(count) + ' different telephone numbers in the records.')
-print(count1)
+
+
+
+"""
+Run-time analysis:
+
+The run time for each for-if loop is O(n). The total run time for all 4 loops when run isolated from each other 
+will be (m+n). Since the constant values are a miniscule determinant in the run time of the overall program
+we can just ignore it and the run time of the entire algorithm will be O(n)
+
+"""
+
+
+
+
