7e54b5f63af0118ec210d936d2dd8b8b552bcc1e
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-06-29 04:20:45 +0500

    task2 dictionary added task-incomplete

--- Task1.py
+++ Task1.py
@@ -20,24 +20,23 @@ Print a message:
 """
 
 different_list = []
-
-for item in calls[0][0]:
-    if item not in different_list:
-        different_list.append(item)
+i = 0
+count = 0
 
 
-for item in calls[0][1]:
-    if item not in different_list:
-        different_list.append(item)
+for call in calls:
+    if call[i][0] not in different_list:
+            different_list.append(call)
+    if call[i][1] not in different_list:
+            different_list.append(call)
 
 
-for item in texts[0][0]:
-    if item not in different_list:
-        different_list.append(item)
+for text in texts:
+    if text[i][0] not in different_list:
+        different_list.append(text)
 
-for item in texts[0][1]:
-    if item not in different_list:
-        different_list.append(item)
+    if text[i][1] not in different_list:
+        different_list.append(text)
         count = len(different_list)
 
 
@@ -48,7 +47,7 @@ print("There are " + str(count) + ' different telephone numbers in the records.'
 """
 Run-time analysis:
 
-The run time for each for-if loop is O(n). The total run time for all 4 loops when run isolated from each other 
+The run time for each for-if loop is O(n). The total run time for both for loops when run isolated from each other 
 will be (m+n). Since the constant values are a miniscule determinant in the run time of the overall program
 we can just ignore it and the run time of the entire algorithm will be O(n)
 
--- Task2.py
+++ Task2.py
@@ -3,11 +3,11 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
-with open('texts.csv', 'r') as f:
+with open('/Users/shehryarbajwa/algorithms-challenges/texts.csv', 'r') as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open('/Users/shehryarbajwa/algorithms-challenges/calls.csv', 'r') as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -20,3 +20,13 @@ Print a message:
 September 2016.".
 """
 
+numbers_dict = {}
+
+for call in calls:
+    if call[0] not in numbers_dict:
+        numbers_dict.update({'number' : call[0], 'duration' : calls[0][3]})
+
+print(numbers_dict)
+
+for key, val in numbers_dict.values():
+    print("Key: " + str(key) + "\t Value:" + str(val))
