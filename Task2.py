52f9c850cfd9db702f5e5e994f72715b8c61969f
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-07-05 01:10:48 +0500

    maxValue incomplete

--- Task2.py
+++ Task2.py
@@ -22,13 +22,28 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+from operator import itemgetter
 
 number_to_call = {}
 for call in calls:
     if call[0] not in number_to_call:
         number_to_call[call[0]]=[]
         number_to_call[call[0]].append([call[1], call[3]])
+        
     else:
         number_to_call[call[0]].append([call[1], call[3]])
+        
+sorted_x = sorted(number_to_call.items(), key=lambda kv: kv[1])
+maxvalue = max(sorted_x)
 
-print(number_to_call)
+print(f'{maxvalue[0]}' + ' spent the longest time,'  + ' seconds,' + ' on the phone during September 2016')
+
+
+
+
+"""
+Call is a list which contains 4 elements
+when we do call in calls
+We are iterating over a list of list
+"""
+
