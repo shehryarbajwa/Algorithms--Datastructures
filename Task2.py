36168150743aaed518d8a26ce8930196c84214f6
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-07-09 01:46:06 +0500

    task2 complete --runtime incomplete

--- Task2.py
+++ Task2.py
@@ -22,8 +22,8 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
-from operator import itemgetter
 
+duration_list = []
 number_to_call = {}
 for call in calls:
     if call[0] not in number_to_call:
@@ -36,10 +36,19 @@ for call in calls:
 sorted_x = sorted(number_to_call.items(), key=lambda kv: kv[1])
 maxvalue = max(sorted_x)
 
-print(f'{maxvalue[0]}' + ' spent the longest time,'  + ' seconds,' + ' on the phone during September 2016')
+"""Extrapolate the duration from the calls and add the value in a seperate list"""
+duration = 0
+i = 0
+while i <= 5:
+    duration_list.append(int(maxvalue[1][i][1]))
+    i += 1
 
 
+"""Adding up the durations together for total number of seconds"""
 
+total_durations = sum(duration_list)
+
+print(f'{maxvalue[0]} ' + ' spent the longest time, ' + f'{total_durations}, seconds' + ' on the phone during September 2016' )
 
 """
 Call is a list which contains 4 elements
