3b507c89324696ef9f2ebd0f38a665926e499c03
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-07-17 00:53:30 +0500

    revision as per reviewer

--- Task2.py
+++ Task2.py
@@ -23,35 +23,67 @@ Print a message:
 September 2016.".
 """
 
-duration_list = []
-number_to_call = {}
+# general_call_dict = {}
+# for call in calls:
+#     #  both aren't entries
+    
+#     if call[0] not in general_call_dict and call[1] not in general_call_dict:
+#         general_call_dict[call[0]] = []
+#         general_call_dict[call[1]] = []
+#         general_call_dict[call[0]].append(int(call[3]))
+#         general_call_dict[call[1]].append(int(call[3]))
+#     #  only outgoing number is entered
+    
+#     # elif call[0] in general_call_dict and call[1] not in general_call_dict:
+#     #     general_call_dict[call[0]].append(int(call[3]))
+#     #     general_call_dict[call[1]] = []
+#     #     general_call_dict[call[1]].append(int(call[3]))
+#     # #  only incoming number is entered
+    
+#     # elif call[0] not in general_call_dict and call[1] in general_call_dict:
+#     #     general_call_dict[call[0]] = []
+#     #     general_call_dict[call[0]].append(int(call[3]))
+#     #     general_call_dict[call[1]].append(int(call[3]))
+    
+#     elif call[0] in general_call_dict and call[1] in general_call_dict:
+#         general_call_dict[call[0]].append(int(call[3]))
+#         general_call_dict[call[1]].append(int(call[3]))
+# print(general_call_dict)
+
+# number_to_minutes = {}
+# for number in general_call_dict:
+#     # number_to_minutes[number] = sum(general_call_dict[number])
+#     call_duration = 0
+#     for iteration in general_call_dict[number]:
+#         call_duration += iteration
+#         number_to_minutes[number] = call_duration
+
+# max_duration = 0
+# for number in number_to_minutes:
+#     if number_to_minutes[number] > max_duration:
+#         max_duration = number_to_minutes[number]
+
+# print('{0} spent the longest time, {1} seconds, on the phone during '
+#       'September 2016.'.format(list(number_to_minutes.keys())[list(
+#         number_to_minutes.values()).index(max_duration)], max_duration))
+
+talk_times = {}
+longest_talker = None
+longest_talk_time = 0
+
 for call in calls:
-    if call[0] not in number_to_call:
-        number_to_call[call[0]]=[]
-        number_to_call[call[0]].append([call[1], call[3]])
-        
-    else:
-        number_to_call[call[0]].append([call[1], call[3]])
-        
-sorted_x = sorted(number_to_call.items(), key=lambda kv: kv[1])
-maxvalue = max(sorted_x)
+    if call[0] not in talk_times:
+        talk_times[ call[0] ] = 0
+    if call[1] not in talk_times:
+        talk_times[ call[1] ] = 0
+    talk_times[ call[0] ] += int( call[3] )
+    talk_times[ call[1] ] += int( call[3] )
 
-"""Extrapolate the duration from the calls and add the value in a seperate list"""
-duration = 0
-i = 0
-while i <= 5:
-    duration_list.append(int(maxvalue[1][i][1]))
-    i += 1
+for talker, time in talk_times.items():
+    if time > longest_talk_time:
+        longest_talker = talker
+        longest_talk_time = time
 
-
-"""Adding up the durations together for total number of seconds"""
-
-total_durations = sum(duration_list)
-
-print(f'{maxvalue[0]} ' + ' spent the longest time, ' + f'{total_durations}, seconds' + ' on the phone during September 2016')
-"""
-Call is a list which contains 4 elements
-when we do call in calls
-We are iterating over a list of list
-"""
-
+print(talk_times)
+print( "{0} spent the longest time, {1} seconds, on the phone during September 2016.".format( 
+    longest_talker, longest_talk_time ) )
