3ebcf8e26a29ba817a6b34a36fdc21a2afe961bb
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-07-19 16:40:36 +0500

    revision as per udacity review

--- Task3.py
+++ Task3.py
@@ -44,19 +44,57 @@ to other fixed lines in Bangalore."
 The percentage should have 2 decimal digits
 """
 
-empty = {}
-empty_2 = {}
+# empty = {}
+# empty_2 = {}
+
+# for call in calls:
+#   if call[0] not in empty:
+#     if call[0][0:5] == '(080)':
+#       empty[call[0]] = call[1]
+#       print(f'The numbers called by people in Bengalore have codes ' + f'{empty[call[0]]}')
+#       if call[1][0:5] == '(080)':
+#         empty_2[call[0]] = call[1]
+
+# length_1 = (len(empty))
+# length_2 = (len(empty_2))
+
+# print(f'{round(length_2 / length_1 , 4) * 100}' + ' percent of calls from fixed lines in Bengalore are calls to other fixed lines in Bengalore.')
+
+def isFromBangalore(number):
+  isfromBengalore = number.startswith('(080)')
+  return isfromBengalore
+
+def getAreaCode(number):
+  # fixed line
+  if number.startswith( '(' ):
+    fixedcode = number.split(')')[0].strip('()')
+    return fixedcode
+  # telemarketer
+  elif number.startswith( '140' ):
+    telemarketer = number.startswith('140')
+    return telemarketer
+  # mobile
+  elif number.find(' ') and number[0] == '7' or number[0] == '8' or number[0] == '9':
+    return number[0:5]                  
+
+codes_bangalore = []
+total_bangalore_calls_count = 0
+local_bangalore_calls_count = 0
 
 for call in calls:
-  if call[0] not in empty:
-    if call[0][0:5] == '(080)':
-      empty[call[0]] = call[1]
-      print(f'The numbers called by people in Bengalore have codes ' + f'{empty[call[0]]}')
-      if call[1][0:5] == '(080)':
-        empty_2[call[0]] = call[1]
+  if isFromBangalore( call[0] ):
+    total_bangalore_calls_count += 1
+    code = getAreaCode( call[1] )
+    if code not in codes_bangalore:
+      codes_bangalore.append( code )
+    if isFromBangalore( call[1] ):
+      local_bangalore_calls_count += 1
 
-length_1 = (len(empty))
-length_2 = (len(empty_2))
+codes_bangalore.sort()
 
-print(f'{round(length_2 / length_1 , 4) * 100}' + ' percent of calls from fixed lines in Bengalore are calls to other fixed lines in Bengalore.')
+print( "The numbers called by people in Bangalore have codes:" )
+for code in codes_bangalore:
+  print( code )
+percentage_local = ( local_bangalore_calls_count / total_bangalore_calls_count ) * 100
 
+print( f"{round(percentage_local,2)} percent of calls from fixed lines in Bangalore are calls to other fixed lines in Bangalore.")
