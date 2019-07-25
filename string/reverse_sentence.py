7446fb03d699b13da8ae0ee8a3f1bdd67973d908
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-07-26 02:22:41 +0500

    reverse Sentence finished

---
+++ string/reverse_sentence.py
@@ -0,0 +1,12 @@
+
+
+def reverse_sentence(our_string):
+    word_list = our_string.split(" ")
+
+    for index in range(len(word_list)):
+        word_list[index] = word_list[index][::-1]
+    
+    return " ".join(word_list)
+
+print(reverse_sentence('Hello World'))
+    
