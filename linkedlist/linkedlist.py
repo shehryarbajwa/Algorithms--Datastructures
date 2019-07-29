8070f855b9eb9f2bda0ff67e186a95422c78777a
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-07-29 09:25:46 +0500

    singly_linked_list --incomplete

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -39,3 +39,20 @@ def create_linked_list(input_list):
 
 create_linked_list([9,3,4,5])
 
+class singly_linked_list:
+    def __init__(self):
+        self.head = None
+        
+    def append(self, value):
+        if self.head is None:
+            self.head = Node(value)
+            return
+        
+        # Move to the tail (the last node)
+        node = self.head
+        while node.next:
+            node = node.next
+        
+        node.next = Node(value)
+        return
+
