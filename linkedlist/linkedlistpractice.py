2222e29306bbf043fe8e5d576efb65adcd5d9fa8
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-08-08 09:35:33 +0500

    append method added

--- linkedlist/linkedlistpractice.py
+++ linkedlist/linkedlistpractice.py
@@ -11,6 +11,7 @@ class Node():
 class LinkedList:
     def __init__(self):
         self.head = None
+        self.tail = None
 
 def prepend(self, value):
 # Prepend a node to the beginning of the list
@@ -22,3 +23,19 @@ def prepend(self, value):
     new_head = Node(value)
     new_head.next = self.head
     self.head = new_head
+
+def append(self, value):
+# Append an element to the list
+
+    if self.head is None:
+        self.head = Node(value)
+        self.tail = self.head
+        return
+    
+
+    node = self.head
+
+    while node.next:
+        node = node.next
+
+    node.next = Node(value)
