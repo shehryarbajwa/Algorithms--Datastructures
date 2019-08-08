bda0feda903cad220dfc1700579552722404b840
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-08-08 09:30:39 +0500

    prepend a linkedlist

--- linkedlist/linkedlistpractice.py
+++ linkedlist/linkedlistpractice.py
@@ -2,3 +2,23 @@
 Linked List practice
 """
 
+
+class Node():
+    def __init__(self,value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+def prepend(self, value):
+# Prepend a node to the beginning of the list
+    
+    if self.head is None :
+        self.head = Node(value)
+        return
+    
+    new_head = Node(value)
+    new_head.next = self.head
+    self.head = new_head
