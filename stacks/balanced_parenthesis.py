d938b7b1364fef2dddb11a0d1dde20d387b310f0
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-09-18 02:54:12 +0500

    stack_balanced_parenthesis complete

--- stacks/balanced_parenthesis.py
+++ stacks/balanced_parenthesis.py
@@ -16,3 +16,48 @@ class Stack:
 
     def size(self):
         return len(self.items)
+
+    def is_empty(self):
+        return self.items == []
+
+#Examples = {()}
+#Example 2 = (()
+#Example 3 = ))
+#Example 4 for else case ))){()}((( - in this case it is already not balanced
+#Example 5 = {{{()}}}
+
+
+def is_matched(p1,p2):
+    if p1 == '(' and p2 == ')':
+        return True
+    elif p1 == '{' and p2 == '}':
+        return True
+    elif p1 == '[' and p2 == ']':
+        return True
+    else:
+        return False
+
+def is_balanced_paren(paren_string):
+
+    index = 0
+    stack = Stack()
+    is_balanced = True
+
+    while index < len(paren_string) and is_balanced:
+        paren = paren_string[index]
+
+        if paren in '{[(':
+            stack.push(paren)
+        else:
+            if stack.is_empty():
+                is_balanced = False
+            else:
+                top = s.pop()
+                if is_matched(top, paren) == False:
+                    is_balanced = False
+        index += 1
+
+    if stack.is_empty() and is_balanced:
+        return True
+    else:
+        return False
