dd7c9cb80a55294eeaa65e8e7f0ea6a21720773c
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-09-18 04:06:11 +0500

    skip i, remove j added

---
+++ linkedlist/skip i, remove j.py
@@ -0,0 +1,4 @@
+""" You are given the head of a linked list and two integers, i and j. 
+    You have to retain the first i nodes and then delete the next j nodes. 
+    Continue doing so until the end of the linked list."""
+
