f41763700f136dc6a4bf7208f35e5becdadaae58
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-09-21 06:22:58 +0500

    handled_queue_excess_elements

--- queue/queue_using_list.py
+++ queue/queue_using_list.py
@@ -7,7 +7,10 @@ class Queue:
         self.front_index = -1
         self.queue_size = 0
 
-    def deque(self, value):
+    def enqueue(self, value):
+        if self.queue_size == len(self.arr):
+            _handle_queue_excess_capaity()
+        
         self.arr[self.next_index] = value
         self.queue_size += 1
         self.next_index = (self.next_index + 1) % len(self.arr)
@@ -24,3 +27,30 @@ class Queue:
         if self.is_empty():
             return None
         return self.arr[self.front_index]
+
+    def dequeue(self, value):
+        if self.is_empty():
+            self.next_index = 0
+            self.front_index = -1
+            return None
+        value = self.arr[self.front_index]
+        self.front_index = (self.front_index + 1) % len(self.arr)
+        self.queue_size -= 1
+        return value
+    
+    def _handle_queue_excess_capaity(self):
+        old_arr = self.arr
+        self.arr = (for _ in range(2 * len(old_arr)))
+
+        index = 0
+
+        for i in range(self.front_index, len(old_arr)):
+            self.arr[index] = old_arr[i]
+            index += 1
+        
+        for i in range(0, self.front_index):
+            self.arr[index] = old_arr[i]
+            index += 1
+        
+        self.front_index = 0
+        self.next_index = index
