c43c523f720a0fa9d7514548bb50f7bd2b5e9a12
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-10-06 06:42:00 +0500

    return subsets instantiated

---
+++ recursion/return_subsets.py
@@ -0,0 +1,26 @@
+def subsets(arr):
+    return return_subsets(arr, 0)
+
+def return_subsets(arr, index):
+    if index >= len(arr):
+        return [[]]
+
+    small_output = return_subsets(arr, index + 1)
+
+    output = list()
+    # append existing subsets
+    for element in small_output:
+        
+        output.append(element)
+
+    # add current elements to existing subsets and add them to the output
+    for element in small_output:
+        current = list()
+        current.append(arr[index])
+        current.extend(element)
+        print(element)
+        print(current)
+        output.append(current)
+    return output
+    
+print(subsets([9]))
