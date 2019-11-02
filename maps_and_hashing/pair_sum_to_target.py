839b131c427113f80aed872493e0931fcdd41f23
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2019-11-02 20:49:40 +0500

    pair_sum_to_target commented

--- maps_and_hashing/pair_sum_to_target.py
+++ maps_and_hashing/pair_sum_to_target.py
@@ -1,16 +1,35 @@
-# Given an input_list and a target, return the indices of pair of integers in the list that sum to the target. 
-# The best solution takes O(n) time. 
-# You can assume that the list does not have any duplicates.
+##########  QUESTION TO ASK THE INTERVIEWER         ##################
+##########  SHOULD TARGET - ELEMENT RETURN JUST 1 PAIR OR MULTIPLE ############
 
-# For e.g. input_list = [1, 5, 9, 7] and target = 8, the answer would be [0, 3]
 
-# The idea is to create a dictionary where we store the index of the elements we iterate over the input_list
-# If we find that the target - iterable element exists in the dictionary, we input that and the index 
-# In our dictionary, we can store the index values of our elements
-# For example in [1,5,9,7] in our dic we store dic[1] -> 0
-#                                              dic[5] -> 1
-#                                              dic[9] -> 2
-#                                              dic[7] -> 3
+#### SO WHAT ARE WE DOING ####
+#   We create an empty dictionary that will map elements(key) to their index in input_list(value)
+#   This is doing the reverse of a usual dictionary where index maps to values
+#   Here values map to index for an array
+#   We will iterate over the original input_list via enumerate, index, element
+#   We will then map the element of the input_list to its index in input_dict
+#   input_dict[element] = index
+#   While we are enumerating
+#   If we find target - element , 5 - 2 = 3 exists in input_dict[key]
+#   Since 3 is included in input_dict after 2 and has a value of 2
+#   We will return index_dict[target - element] = 2, index we are iterating over 1
+#   Then we will return the input_dict[element] against that value which is 
+#   
+#    {
+#    '1':'0',
+#    '2':'1',
+#    '3':'2',
+#    '4':'3'
+#   }
+
+#   
+
+
+
+
+
+
+
 
 def pair_to_sum_target(input_list, target):
     index_dict = dict()
@@ -20,7 +39,4 @@ def pair_to_sum_target(input_list, target):
             return [index_dict[target - element], index]
         index_dict[element] = index 
 
-#   In our algorithm, when we are iterating over for index(3), element = 7
-#   Our target is 8, and 8 - 7 = 1
-#   since we have 1 in index_dict as a key value
-#   
+print(pair_to_sum_target([1,5,9,7], 8))
