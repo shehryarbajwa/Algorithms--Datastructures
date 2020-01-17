34bc2329731ef0e7fc4796ce8e5d7026e8e0d019
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-01-17 00:04:27 -0500

    post_order traversal added

---
+++ algoexpert/medium/in_pre_post_ordertraversal.py
@@ -0,0 +1,75 @@
+#                             10
+#                           /   \
+#                         5       15
+#                       /  \        \
+#                      2    5       22
+#                    /       \      / \
+#                   1         6    20  24  
+
+# In-order = [1,2,5,5,6,10,15,20,22,24]
+# In in-order, we first get to the left basecase which are the leafs.
+# Then we append the currentValue and then traverse on the right side
+
+# We start with 10 -> 5 -> 2 -> 1 -> [1]
+# We come back up, since we have appended and no right value for 2 so [1,2]
+# We come back up, 5's left is [1,2] and now current value is 5 so [1,2,5]
+# 5's right there is no left so we append 5 and then append 6 [1,2,5,5,6]
+# Come back up again at 10's left is [1,2,5,5,6] so [1,2,5,5,6,10]
+# Recurse on 10's right -> 15
+# 15 has no left so append 15
+# Recurse 15's right so [1,2,5,5,6,10,15]
+# 22 has left so we get 20 first [1,2,5,5,6,10,15,20]
+# Then go back since 20 is leaf.
+# 22's left is [20]
+# Then append 22's current value
+# [1,2,5,5,6,10,15,20,22]
+# Finally we recurse to the right [1,2,5,5,6,10,15,20,22,24]
+# We run this tree is not None
+# Once we finish we return array
+
+def in_order_traversal(tree, array):
+    if tree is not None:
+        in_order_traversal(tree.left, array)
+        array.append(tree.value)
+        in_order_traversal(tree.right, array)
+    else:
+        return 
+    
+    return array
+
+#                             10
+#                           /   \
+#                         5       15
+#                       /  \        \
+#                      2    5       22
+#                    /       \      / \
+#                   1         6    20  24  
+
+# Pre-order we start with root.
+# Then recurse left and then right
+# [10, 5, 2, 1, 5, 6, 15, 22, 20, 24 ]
+def pre_order_tree(tree, array):
+    if tree is not None:
+        array.append(tree.value)
+        pre_order_tree(tree.left, array)
+        pre_order_tree(tree.right, array)
+    else:
+        return 
+
+    return array
+
+#[1,2,6,5,5,20, 24, 22, 15,10]
+def post_order_tree(tree, array):
+    if tree is not None:
+        post_order_tree(tree.left, array)
+        post_order_tree(tree.right, array)
+        array.append(tree.value)
+
+    else:
+        return 
+    
+    return array
+
+#In-order means in the middle add to array
+#Pre-order means add it to the array before recursing left or right
+#Post-order means add it to the array after recursing left and right
--- algoexpert/medium/validate_binary_tree.py
+++ algoexpert/medium/validate_binary_tree.py
@@ -33,31 +33,38 @@ def validateBSThelper(currentNode, minimum, maximum):
 	
 	if currentNode is None:
 		return True
-	
+	print('Got here')
 	leftSide = leftSideHelper(currentNode.left, minimum, currentNode.value)
 	rightSide = rightSideHelper(currentNode.right, currentNode.value, maximum)
 	return leftSide and rightSide
 
 def leftSideHelper(leftNode, minimum, parentNode):
-	if leftNode is None:
-		return True
+    print(leftNode.value)
+    print('Parent node is', parentNode)
+    if leftNode is None:
+        return True
 
-	if leftNode.value >= parentNode:
-		return False
-
-	if leftNode.value < minimum:
-		return False
+    if leftNode.value >= parentNode:
+        return False
+    elif leftNode.value < minimum:
+        return False
+    else:
+        return True
 	
 def rightSideHelper(rightNode, parentNode, maximum):
+    print(rightNode.value)
+    print('Parent node is', parentNode)
     if rightNode is None:
         return True
 
     if rightNode.value <= parentNode:
         return False
 
-    if rightNode.value > maximum:
+    elif rightNode.value > maximum:
         return False
+    else:
+        return True
 	
 
-BST_1 = BST(10).insert(5).insert(15)
+BST_1 = BST(10).insert(5).insert(15).insert(5).insert(2).insert(1).insert(22)
 print(validateBst(BST_1))
