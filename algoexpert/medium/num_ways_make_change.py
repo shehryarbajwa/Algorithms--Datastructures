0c558b733481fbe794e87ab1b042413e4237c853
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-02-18 19:09:37 -0500

    updated comments

--- algoexpert/medium/num_ways_make_change.py
+++ algoexpert/medium/num_ways_make_change.py
@@ -4,18 +4,23 @@
 #Base case - When making change for 0, there is only one way and that is to not offer any change
 #            This can be done by making no change and buying it for free.
 
+## Hint: Build up the array one coin denomination at a time. In other words, find the number of ways to make change for all amounts between 0 and n.
+## Basically iterate over the target array called ways.
+## At each list item, iterate over the denomination, and if the denomination is equal to the amount, then increment that array with the difference between amount and denom.
 
-def numberOfWaysToMakeChange(n, denoms):
-    ways = [0 for amount in range(n + 1)]
-    print('ways is ', ways)
+# ways = _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+# this   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+
+
+def numberofwaystomakechange(target, array):
+
+    ways = [0 for amount in range(target + 1)]
     ways[0] = 1
 
-    for denom in denoms:
-        for amount in range(1, n + 1):
+    for denom in array:
+        for amount in range(1, target + 1):
             if denom <= amount:
                 ways[amount] += ways[amount - denom]
-    
-    return ways[n]
 
+    return ways[target]
 
-print(numberOfWaysToMakeChange(10, [1,5,10,25]))
