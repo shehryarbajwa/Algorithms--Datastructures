0b9909453395169bc1f3369344890edeb4278046
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-03-05 01:30:06 -0500

    added using adjacency list

---
+++ algoexpert/medium/bfs-graph.py
@@ -0,0 +1,33 @@
+
+#Adjacency List
+graph = {'A': ['B', 'C', 'E'],
+         'B': ['A', 'D','E'],
+         'C': ['F', 'G' , 'A'],
+         'D': ['B', 'E'],
+         'E': ['A', 'B','D'],
+         'F': ['C'],
+         'G': ['C']}
+
+def bfs_connected_component(graph, start):
+    # keep track of all visited nodes
+    explored = []
+    # keep track of nodes to be checked
+    queue = [start]
+ 
+    # keep looping until there are nodes still to be checked
+    while queue:
+        # pop shallowest node (first node) from queue
+        node = queue.pop(0)
+        if node not in explored:
+            # add node to list of checked nodes
+            explored.append(node)
+            neighbours = graph[node]
+ 
+            # add neighbours of node to queue
+            for neighbour in neighbours:
+                queue.append(neighbour)
+    return explored
+ 
+print(bfs_connected_component(graph,'A'))
+
+
--- algoexpert/medium/bfs-node.py
+++ algoexpert/medium/bfs-node.py
@@ -1,21 +1,34 @@
+import unittest
+
+
+#Time Complexity O(V + E)
+#Traverse all nodes at one point
+
 class Node:
     def __init__(self, value):
         self.value = value
         self.children = []
 
-    
     def addChild(self, value):
         self.children.append(Node(value))
         return self
+
     
+    #1-Add the vertex to start the breadthFirstSearch to the empty queue
+    #2-Mark that vertex visited
+    #3-Extract a vertex from the queue and add its neighbors to the queue if that isnt marked visited
+    #4-Repeat step 3 until the queue is empty
+
     def breadthFirstSearch(self, bfsarray):
+        #Add the first vertex. Self refers to the first value of the graph/tree
         queue = [self]
         while len(queue) > 0:
+            #Get the first element of the tree
             current = queue.pop(0)
+            #Add it to viisted array
             bfsarray.append(current.value)
+            #Add its children to the queue
             for child in current.children:
                 queue.append(child)
+            #keep running the loop till the queue becomes emtpy
         return bfsarray
-
-
-
