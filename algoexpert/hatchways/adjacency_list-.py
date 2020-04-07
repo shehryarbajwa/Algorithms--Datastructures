362a68ea2be9da4d6a9d422ffd902680b195516a
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-04-07 06:05:49 -0400

    create tests for graph

--- algoexpert/hatchways/adjacency_list-.py
+++ algoexpert/hatchways/adjacency_list-.py
@@ -1,3 +1,5 @@
+import unittest
+
 class Graph:
     def __init__(self, vertices, is_directed=False):
         self.vertices = vertices
@@ -29,7 +31,57 @@ class Graph:
     def print_adjacency_list(self):
         for node in self.adjacency_list:
             print(node + ' -> ' + self.adjacency_list[node])
-    
+
+    def degree(self, vertex):
+        degree = len(self.adjacency_list[vertex])
+        return degree
+
     
     
 
+    
+class Tests(unittest.TestCase):
+    def test_case_1(self):
+        nodes = ["A","B","C","D","E"]
+        graph = Graph(nodes, True)
+        graph.add_edge('A', 'B')
+        graph.add_edge('B', 'C')
+        graph.add_edge('C', 'D')
+        graph.add_edge('E', 'D')
+        graph.add_edge('E', 'A')
+        self.assertEqual(graph.contains('A','E'), False)
+        self.assertEqual(graph.contains('A','B'), True)
+
+    def test_case_2(self):
+        nodes = ["A","B","C","D","E"]
+        graph = Graph(nodes, True)
+        graph.add_edge('A', 'B')
+        graph.add_edge('B', 'C')
+        graph.add_edge('C', 'D')
+        graph.add_edge('E', 'D')
+        graph.add_edge('E', 'A')
+        self.assertEqual(graph.degree('E'),2)
+    
+    def test_case_3(self):
+        nodes = ["A","B","C","D","E"]
+        graph = Graph(nodes, True)
+        graph.add_edge('A', 'B')
+        graph.add_edge('B', 'C')
+        graph.add_edge('C', 'D')
+        graph.add_edge('E', 'D')
+        graph.add_edge('E', 'A')
+        self.assertEqual(graph.degree('A'),1)
+
+    def test_case_4(self):
+        nodes = ["A","B","C","D","E"]
+        graph = Graph(nodes, False)
+        graph.add_edge('A', 'B')
+        graph.add_edge('B', 'C')
+        graph.add_edge('C', 'D')
+        graph.add_edge('E', 'D')
+        graph.add_edge('E', 'A')
+        self.assertEqual(graph.degree('A'),2)
+
+
+if __name__ == '__main__':
+    unittest.main()
