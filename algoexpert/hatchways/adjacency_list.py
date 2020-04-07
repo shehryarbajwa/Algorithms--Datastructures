4ab0fd85593ee5028ab85140ae6cb90e3ccc0ec6
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-04-07 06:06:18 -0400

    renamed adjacency list

--- algoexpert/hatchways/adjacency_list.py
+++ algoexpert/hatchways/adjacency_list.py
@@ -1,31 +1,24 @@
+import unittest
 
 class Graph:
-    def __init__(self, Nodes, is_directed=False):
-        self.nodes = Nodes
+    def __init__(self, vertices, is_directed=False):
+        self.vertices = vertices
         self.adjacency_list = {}
         self.is_directed = is_directed
 
-        for node in self.nodes:
-            self.adjacency_list[node] = []
-
-    def print_adjacency_list(self):
-        for node in self.nodes:
-            print(node, '->', self.adjacency_list[node])
+        for v in vertices:
+            self.adjacency_list[v] = []
 
     def add_edge(self, source, destination):
         self.adjacency_list[source].append(destination)
-        #if undirected, map destination to source aswell
+
         if not self.is_directed:
             self.adjacency_list[destination].append(source)
 
-    def degree(self, node):
-        degree  = len(self.adjacency_list[node])
-        return degree
+    def contains(self, source, destination):
 
-    def contains(self, node, destination):
-
-        for vertex in self.adjacency_list[node]:
-            if vertex == destination:
+        for v in self.adjacency_list[source]:
+            if v == destination:
                 return True
         return False
 
@@ -35,43 +28,60 @@ class Graph:
         if not self.is_directed:
             self.adjacency_list[destination].remove(source)
 
-    def BFS(self, root):
+    def print_adjacency_list(self):
+        for node in self.adjacency_list:
+            print(node + ' -> ' + self.adjacency_list[node])
 
-        visited = [False for _ in range(len(self.adjacency_list))]
-        queue = [root]
+    def degree(self, vertex):
+        degree = len(self.adjacency_list[vertex])
+        return degree
 
-        while queue:
-            current = queue.pop(0)
-            visited.append(current)
-            for child in self.adjacency_list[current]:
-                queue.append(child)
-        return visited
-            
-            
+    
+    
+
+    
+class Tests(unittest.TestCase):
+    def test_case_1(self):
+        nodes = ["A","B","C","D","E"]
+        graph = Graph(nodes, True)
+        graph.add_edge('A', 'B')
+        graph.add_edge('B', 'C')
+        graph.add_edge('C', 'D')
+        graph.add_edge('E', 'D')
+        graph.add_edge('E', 'A')
+        self.assertEqual(graph.contains('A','E'), False)
+        self.assertEqual(graph.contains('A','B'), True)
+
+    def test_case_2(self):
+        nodes = ["A","B","C","D","E"]
+        graph = Graph(nodes, True)
+        graph.add_edge('A', 'B')
+        graph.add_edge('B', 'C')
+        graph.add_edge('C', 'D')
+        graph.add_edge('E', 'D')
+        graph.add_edge('E', 'A')
+        self.assertEqual(graph.degree('E'),2)
+    
+    def test_case_3(self):
+        nodes = ["A","B","C","D","E"]
+        graph = Graph(nodes, True)
+        graph.add_edge('A', 'B')
+        graph.add_edge('B', 'C')
+        graph.add_edge('C', 'D')
+        graph.add_edge('E', 'D')
+        graph.add_edge('E', 'A')
+        self.assertEqual(graph.degree('A'),1)
+
+    def test_case_4(self):
+        nodes = ["A","B","C","D","E"]
+        graph = Graph(nodes, False)
+        graph.add_edge('A', 'B')
+        graph.add_edge('B', 'C')
+        graph.add_edge('C', 'D')
+        graph.add_edge('E', 'D')
+        graph.add_edge('E', 'A')
+        self.assertEqual(graph.degree('A'),2)
 
 
-
-nodes = ["A","B","C","D","E"]
-graph = Graph(nodes, True)
-graph.add_edge('A', 'B')
-graph.add_edge('B', 'C')
-graph.add_edge('C', 'D')
-graph.add_edge('E', 'D')
-graph.add_edge('E', 'A')
-print(graph.BFS('A'))
-
-
-graph.print_adjacency_list()
-print(graph.degree('E'))
-print(graph.contains('E', 'A'))
-# nodes = ["A","B","C","D","E"]
-# graph = Graph(nodes)
-# graph.add_edge('A', 'B')
-# graph.add_edge('B', 'C')
-# graph.add_edge('C', 'D')
-# graph.add_edge('D', 'E')
-# graph.add_edge('E', 'A')
-# graph.print_adjacency_list()
-
-
-
+if __name__ == '__main__':
+    unittest.main()
