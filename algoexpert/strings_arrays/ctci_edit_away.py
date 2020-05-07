b941edf24e0224e0452b9483d3e7b594978a62e9
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-05-07 06:51:22 -0400

    updated edit away

--- algoexpert/strings_arrays/ctci_edit_away.py
+++ algoexpert/strings_arrays/ctci_edit_away.py
@@ -1,33 +1,8 @@
+import unittest
 
-def check_edit_away(s1, s2):
-    if (len(s1) != len(s2)):
-        is_edit_away_insertion_deletion(s1, s2)
-    
-    insertion_hash = {}
-
-    for char in s1:
-        if char in insertion_hash:
-            insertion_hash[char] += 1
-        else:
-            insertion_hash[char] = 1
-
-    for char in s2:
-        if char in insertion_hash:
-            insertion_hash[char] -= 1
-        else:
-            insertion_hash[char] = 1
-
-    count = 0
-    value_list = list(insertion_hash.values())
-    
-    for i in range(len(value_list)):
-        if value_list[i] == abs(1):
-            count += 1
-    
-    return count <= 2
-
-
-def is_edit_away_insertion_deletion(s1, s2):
+#Time Complexity O(N)
+#Space Complexity O(N) since hash table might contain all n's
+def is_edit_away(s1, s2):
 
     difference = abs(len(s1) - len(s2))
 
@@ -49,11 +24,32 @@ def is_edit_away_insertion_deletion(s1, s2):
             insertion_hash[char] = 1
     count = 0
     value_list = list(insertion_hash.values())
-    print(insertion_hash)
 
     for i in range(len(value_list)):
         if value_list[i] == abs(1):
             count += 1
-    return count <= 1
+    
+    if difference == 1:
+        return count <= 1
+    else:
+        return count <= 2
 
-print(check_edit_away('pale', 'bake'))
+
+class Tests(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(is_edit_away('sale','sle'), True)
+
+    def test_case_2(self):
+        self.assertEqual(is_edit_away('sale','male'), True)
+
+    def test_case_3(self):
+        self.assertEqual(is_edit_away('sales','sale'), True)
+
+    def test_case_4(self):
+        self.assertEqual(is_edit_away('salesss','sale'), False)
+
+    def test_case_5(self):
+        self.assertEqual(is_edit_away('sales','1234'), False)
+
+if __name__ == '__main__':
+    unittest.main()
