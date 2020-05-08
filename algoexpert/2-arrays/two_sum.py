9de60b69d6d1e0bf4d0234f150a072e66172538d
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-05-08 01:04:39 -0400

    two sum added

---
+++ algoexpert/2-arrays/two_sum.py
@@ -0,0 +1,26 @@
+
+import unittest
+
+def two_sum(array, target):
+
+    sum_map = {}
+
+    for num in array:
+        if target - num in sum_map:
+            return [target - num, num]
+        else:
+            sum_map[num] = True
+    return []
+
+class Tests(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(two_sum([1,2,3,4,5,6,7,8,9,10], 10), [4,6])
+
+    def test_case_2(self):
+        self.assertEqual(two_sum([1,2,3,4,5,6,7,8,9,10], 5), [2,3])
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
