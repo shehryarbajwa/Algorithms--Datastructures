59c127276d96c15c27d1a4a6ece1f0e02621396c
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-05-09 15:58:37 -0400

    monotonic array added

---
+++ algoexpert/2-arrays/monotonic_array.py
@@ -0,0 +1,37 @@
+
+
+
+def is_monotonic(array):
+
+    increasing_sequence = True
+    decreasing_sequence = True
+
+    for i in range(1, len(array)):
+        if array[i] < array[i - 1]:
+            decreasing_sequence = True
+            increasing_sequence = False
+
+        if array[i] > array[i - 1]:
+            increasing_sequence = True
+            decreasing_sequence = False
+
+    return decreasing_sequence or increasing_sequence
+
+def direction_of_array(array):
+
+    increasing = True
+    left = 0
+    right = left + 1
+
+    while increasing and right < len(array):
+        if array[right] > array[left]:
+            left += 1
+            right += 1
+        else:
+            increasing = False
+    return increasing
+        
+
+print(is_monotonic([-1,-5,-10,-1100,-1101,-1102, -9001]))
+
+print(direction_of_array([1, 5, 4, 1100, 1101, 1102, 9001]))
