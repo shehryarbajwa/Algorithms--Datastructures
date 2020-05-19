59dd8da25d525c331c4b5e967f5e7e70651544e4
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-05-19 02:38:07 -0400

    kth element from linked list remove

---
+++ algoexpert/3-linked_list/remove_kth_from_end.py
@@ -0,0 +1,31 @@
+class LinkedList:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+def remove_kth_node(head, k):
+    current, p1 = head, head.next
+
+    while current is not None and p1.value is not k:
+        current = current.next
+        p1 = p1.next
+    
+    current.next = p1.next
+
+LL = LinkedList(1)
+LL.next = LinkedList(2)
+LL.next.next = LinkedList(3)
+LL.next.next.next = LinkedList(4)
+
+print(LL.value)
+print(LL.next.value)
+print(LL.next.next.value)
+print(LL.next.next.next.value)
+
+remove_kth_from_end(LL, 2)
+
+print(LL.value)
+print(LL.next.value)
+print(LL.next.next.value)
+
+
