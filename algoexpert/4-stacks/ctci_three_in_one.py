0a5d99ada450e18ec11294334b2f8e6dd8ffa5c0
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-05-28 17:45:38 -0400

    three in one added

--- algoexpert/4-stacks/ctci_three_in_one.py
+++ algoexpert/4-stacks/ctci_three_in_one.py
@@ -1,40 +1,53 @@
-class ThreeOneStack:
+class MultiStack:
+    
     def __init__(self, stacksize):
-        self.numstacks = 3
-        self.array = [0] * (stacksize * self.numstacks)
-        self.sizes = [0] * self.numstacks
-        self.stacksize = stacksize
+        self.stacknum = 3
+        self.stackcapacity = stacksize
+        self.array = [0 for _ in range(stacksize * self.stacknum)]
+        self.size = [0] * self.stacknum
 
     def push(self, item, stacknum):
-        if self.isFull(stacknum):
-            raise Exception('Stack is full')
-        self.sizes[stacknum] += 1
-        self.array[self.IndexOfTop(stacknum)] = item
+        if self.is_full(stacknum):
+            raise ValueError("Stack is full")
+        self.size[stacknum] += 1
+        self.array[self.indexTop(stacknum)] = item
 
     def pop(self, stacknum):
-        if self.isEmpty(stacknum):
-            raise Exception('Stack is empty')
-        value = self.array[self.IndexofTop(stacknum)]
-        self.array[self.IndexOfTop(stacknum)] = 0
-        self.sizes[stacknum] -= 1
+        if self.is_empty(stacknum):
+            raise ValueError("Stack is empty")
+        value = self.array[self.indexTop(stacknum)]
+        self.array[self.indexTop(stacknum)] = 0
+        self.size[stacknum] -= 1
         return value
 
-    def peek(self, stacknum):
-        if self.isEmpty(stacknum):
-            raise Exception('Stack is empty')
-        return self.array[self.IndexOfTop(stacknum)]
 
     def is_empty(self, stacknum):
-        return self.sizes[stacknum] == 0
+        return self.size[stacknum] == 0
 
     def is_full(self, stacknum):
-        return self.sizes[stacknum] == self.stacksize
+        return self.size[stacknum] == self.stackcapacity
 
-    def IndexOfTop(self, stacknum):
-        offset = stacknum * self.stacksize
-        return offset + self.sizes[stacknum] - 1
+    def indexTop(self, stacknum):
+        offset = stacknum * stackcapacity
+        size = self.size[stacknum]
+        return offset + size - 1
+
+
+    
+
 
        
 
-three = ThreeOneStack()
-three.push(3)+.0
+def ThreeInOne():
+    newstack = MultiStack(2)
+    print(newstack.IsEmpty(1))
+    newstack.Push(3, 1)
+    print(newstack.Peek(1))
+    print(newstack.IsEmpty(1))
+    newstack.Push(2, 1)
+    print(newstack.Peek(1))
+    print(newstack.Pop(1))
+    print(newstack.Peek(1))
+    newstack.Push(3, 1)
+
+ThreeInOne()
