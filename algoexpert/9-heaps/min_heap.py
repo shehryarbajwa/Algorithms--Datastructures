13ae95090928975037bf2e422638538b3729c1fa
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-06-24 19:23:13 -0400

    initialize min_heap class

---
+++ algoexpert/9-heaps/min_heap.py
@@ -0,0 +1,21 @@
+class MinHeap:
+    def __init__(self, array):
+        self.heap = self.build_heap(array)
+
+    def build_heap(self, array):
+        pass
+
+    def sift_down(self):
+        pass
+
+    def sift_up(self):
+        pass
+
+    def peek(self):
+        return self.heap[0]
+
+    def remove(self):
+        pass
+
+    def insert(self, value):
+        pass
