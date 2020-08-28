40e87a13bcbd7826215e956ab1d67b5a5aabd161
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-08-27 22:22:28 -0400

    distinct pairs added

---
+++ algoexpert/2-arrays/distinct_pairs.py
@@ -0,0 +1,26 @@
+
+def distinct_pairs(array, target):
+    array.sort()
+    left = 0
+    right = len(array) - 1
+    count = 0
+
+    while left < right:
+        if array[left] + array[right] == target:
+            count += 1
+            while left < right - 1 and array[left] == array[left + 1]:
+                left += 1
+            left += 1
+
+            while right > left and array[right] == array[right - 1]:
+                right -= 1
+            right -= 1
+
+        elif array[left] + array[right] > target:
+            right -= 1
+
+        else:
+            left += 1
+    return count
+
+print(distinct_pairs([1,3,46, 1, 3, 9],47))
