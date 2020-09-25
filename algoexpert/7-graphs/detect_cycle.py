f5ec765971695cd44f762b4db2b423d96cbbeb26
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-09-25 00:30:18 -0400

    detect cycle

--- algoexpert/7-graphs/detect_cycle.py
+++ algoexpert/7-graphs/detect_cycle.py
@@ -2,6 +2,21 @@ def detect_cycle(graph, start):
     visited = set()
 
     def recursive_helper(current_vertex, parent):
-        pass
+        visited.add(current_vertex)
 
-    for i in range(len(self.adjacency_list)):
+        for neighbor in graph[current_vertex]:
+            if neighbor not in visited:
+                if recursive_helper(neighbor, current_vertex):
+                    return True
+            elif neighbor != parent:
+                return True
+        return False
+
+    for i in range(len(graph)):
+        if i not in visited:
+            if recursive_helper(i, -1):
+                return True
+    return False
+    
+
+    
