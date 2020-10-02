a5c0c28c63e15657a2bfc171b373ffdfd539823e
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-10-02 13:51:02 -0400

    multi-string search

---
+++ tries/multi_string_search.py
@@ -0,0 +1,23 @@
+class Trie(object):
+    def __init__(self):
+        self.root = {}
+        self.end_symbol = '*'
+
+
+    def add_word(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                current[letter] = {}
+            current = current[letter]
+        current[self.end_symbol] = True
+
+    def contains(self, word):
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                return False
+            current = current[letter]
+        return self.end_symbol in current
+
+    
--- tries/suffix_trie.py
+++ tries/suffix_trie.py
@@ -8,6 +8,8 @@ class Trie:
         for i in range(len(string)):
             self.build_tree(i, string)
 
+    #Creation is O(N^2) since N^2 is using double for loops for finding suffixes for each string
+    #Space Complexity is O(N^2) since we are storing each suffix for each word
     def build_tree(self, index, string):
         current = self.root
         for i in range(index, len(string)):
@@ -17,6 +19,8 @@ class Trie:
             current = current[letter]
         current[self.end_symbol] = True
 
+    #Search is O(M) time complexity
+    #Search is O(1) space complexity
     def contains(self, string):
         current = self.root
         for letter in string:
