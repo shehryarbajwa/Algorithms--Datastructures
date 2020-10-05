326b9e076b021a51ca1437dc3af7eaa0c74b06b0
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-10-05 19:39:33 -0400

    kth largest in a stream

---
+++ heaps/kth_largest_stream.py
@@ -0,0 +1,21 @@
+import heapq
+class KthLargest:
+    def __init__(self, k, nums):
+        self.array = nums
+        self.k = k
+
+        heapq.heapify(self.array)
+
+        if len(self.array) > k:
+            heapq.heappop(self.array)
+
+    def add(self, value):
+        heap_top = 0
+
+        if len(self.array) < self.k:
+            heapq.heappush(self.array, value)
+        else:
+            heapq.heappush(self.array, value)
+            heapq.heappop(self.array)
+        return self.array[heap_top]
+
