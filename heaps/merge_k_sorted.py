31918958283d503fe259721478b68a86f46b822e
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-10-08 01:49:59 -0400

    merge sorted

--- heaps/merge_k_sorted.py
+++ heaps/merge_k_sorted.py
@@ -1,25 +1,37 @@
-# def merge_sorted_lists(array):
-
-
-
+def merge_sorted_lists(array):
+    un_sorted_list = []
+    sorted_list = []
+    for i in range(len(array)):
+        j = 0
+        while j < len(array[i]):
+            un_sorted_list.append(array[i][j])
+            j += 1
+    
+    heap = Heaps(un_sorted_list)
+    while not heap.is_empty():
+        sorted_list.append(heap.remove())
+    return sorted_list
 
 class Heaps:
     def __init__(self, array):
         self.heap = self.build_heap(array)
 
+    def is_empty(self):
+        return len(self.heap) == 0
 
     def build_heap(self, array):
         first_parent_idx = (len(array) - 2) // 2
-        for idx in reversed(range(first_parent_idx + 1)):
-            self.sift_down(idx, len(array) - 1, array)
+
+        #Go from end all way to top
+        for current_idx in reversed(range(first_parent_idx)):
+            self.sift_down(current_idx, len(array) - 1, array)
         return array
 
     def sift_down(self, current_idx, end_idx, heap):
         child_one_idx = current_idx * 2 + 1
-
-        while current_idx <= end_idx:
-            child_two_idx = current_idx * 2 + 2 if current_idx * 2 + 2 else -1
-
+        while child_one_idx <= end_idx:
+            child_two_idx = current_idx * 2 + 2 if current_idx * 2 + 2 <= end_idx else -1
+            #If child_two is smaller than child one then we swap with child two
             if child_two_idx != -1 and heap[child_two_idx] < heap[child_one_idx]:
                 idx_to_swap = child_two_idx
             else:
@@ -31,16 +43,17 @@ class Heaps:
                 child_one_idx = current_idx * 2 + 1
             else:
                 return
-
-
+    
     def sift_up(self, current_idx, heap):
         parent_idx = (current_idx - 1) // 2
+
         while current_idx > 0 and heap[current_idx] < heap[parent_idx]:
-            self.swap(current_idx, parent_idx, heap)
+            self.swap(current_idx, parent_idx, self.heap)
             current_idx = parent_idx
             parent_idx = (current_idx - 1) // 2
 
 
+    #Remove root value - minimum value
     def remove(self):
         self.swap(0, len(self.heap) - 1, self.heap)
         value_to_remove = self.heap.pop()
@@ -49,11 +62,11 @@ class Heaps:
 
     def insert(self, value):
         self.heap.append(value)
-        self.sift_up(len(self.heap) - 1, self.heap)
+        self.sift_up(len(self.heap) -  1, self.heap)
 
     def swap(self, i, j, heap):
         heap[i], heap[j] = heap[j], heap[i]
 
 
             
-        
+print(merge_sorted_lists([[1, 5, 9, 21], [-1, 0], [-124, 81, 121], [3, 6, 12, 20, 150]]))
