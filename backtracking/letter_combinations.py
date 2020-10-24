e429e8afb8872e4aa6dcd100299af75eda15ac17
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-10-24 16:49:04 -0400

    letter combinations added

---
+++ backtracking/letter_combinations.py
@@ -0,0 +1,34 @@
+def letter_combinations(digits):
+    if not digits:
+        return []
+    output = []
+    recursive_letter_combination(digits, '', output)
+    return output
+
+def recursive_letter_combination(digits, current_string, output):
+    #base case
+    if len(digits) == 0:
+        output.append(current_string)
+        return
+    
+    characters = digit_to_letter(digits[0])
+    for character in characters:
+        current_string += character
+        recursive_letter_combination(digits[1:], current_string, output)
+        #backtrack, decrement the character added
+        #move back up
+        current_string = current_string[:-1]
+
+def digit_to_letter(digit):
+    mapping = {
+        '2' :'abc',
+        '3' :'def',
+        '4' :'ghi',
+        '5' :'jki',
+        '6' :'mno',
+        '7' :'pqrs',
+        '8' :'tuv',
+        '9' :'wxyz'
+    }
+    
+    return mapping[digit]
