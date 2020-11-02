dbcfeb2d9581ae3b43eaa414e2d4e73f6406bd9f
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-11-02 02:36:21 -0500

    combinations added

---
+++ backtracking/combinations.py
@@ -0,0 +1,14 @@
+#Time Complexity O(K * NCK) - k for hitting base case, NCK for selecting 4Combination 2 selections
+def combinations(n, k):
+    output = []
+    def backtrack(start, current_combination):
+        if len(current_combination) == k:
+            output.append(current_combination)
+        else:
+            for i in range(start, n + 1):
+                current = current_combination + [i]
+                backtrack(i + start, current)
+    backtrack(1, [])
+    return output
+
+print(combinations(4, 2))
