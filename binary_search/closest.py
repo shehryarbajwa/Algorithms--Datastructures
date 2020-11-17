8e2de9ebd3d863465c1ddd296e726449a7f34c75
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-11-17 02:23:18 -0500

    find closest number added

--- binary_search/closest.py
+++ binary_search/closest.py
@@ -1,6 +1,4 @@
-
-
-def binary_search(array, target):
+def binary_search_closest(array, target):
     left = 0
     right = len(array) - 1
 
@@ -12,10 +10,13 @@ def binary_search(array, target):
             left = mid
         else:
             right = mid
-    if array[left] == target:
-        return left
-    if array[right] == target:
-        return right
-    return -1
 
-print(binary_search([1,2,2,4,5,5,8], 5))
+
+    #Post processing
+
+    if abs(target - array[left]) < abs(target - array[right]):
+        return left
+    else:
+        return right
+
+print(binary_search_closest([1,2,3,6,7],100))
