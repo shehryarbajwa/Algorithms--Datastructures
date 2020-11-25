27bb24a67ec8465ee895438ec208dc44ef2119ad
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-11-24 23:39:39 -0500

    num_leaves added

--- binary_search/binary_tree/height_binary_tree.py
+++ binary_search/binary_tree/height_binary_tree.py
@@ -3,3 +3,12 @@ def height(root):
         return 0
     return 1 + max(height(root.left), height(root.right))
 
+def num_leaves(root):
+    if not root:
+        return 0
+
+    if not root.left and not root.right:
+        return 1
+    return num_leaves(root.left) + num_leaves(root)
+    
+
