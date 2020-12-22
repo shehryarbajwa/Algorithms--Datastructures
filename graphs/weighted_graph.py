8f25e9f75480e5da963f5e791084a69ccbd5abcf
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-12-22 19:49:45 +0500

    adjacency matrix added

--- graphs/weighted_graph.py
+++ graphs/weighted_graph.py
@@ -1,18 +1,17 @@
 def create_adj_list(edges, nodes):
     graph = [[] for _ in range(nodes)]
 
-    for u, v in edges:
-        graph[u].append(v)
+    for u, v, w in edges:
+        graph[u].append((v, w))
+        graph[v].append((u, v))
 
     return graph
-
-print(create_adj_list([(0,1),(1,2)],3))
-
 
 def create_adj_matrix(edges, nodes):
     graph = [[None for _ in range(nodes)] for _ in range(nodes)]
 
-    for u, v in edges:
-        graph[u][v] = True
+    for u, v, w in edges:
+        graph[u][v] = w
+        graph[v][u] = w
 
     return graph
