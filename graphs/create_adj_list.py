0437dc08fdd165133a3a8cbeb661b9153b7a9812
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-12-22 19:26:00 +0500

    create_adj list added

--- graphs/create_adj_list.py
+++ graphs/create_adj_list.py
@@ -1,11 +1,21 @@
-def create_adj_list(n, edge_list):
+def create_adj_list(edges, amount_of_vertices):
 
-    G = [[] for v in range(n + 1)]
+    graph = [[] for _ in range(amount_of_vertices)]
 
-    for u, v in edge_list:
-        G[u].append(v)
-        G[v].append(u)
-        
-    return G[1:]
+    for u,v in edges:
+        graph[u].append(v)
+        graph[v].append(u)
+    return graph
 
-print(create_adj_list(3, [[2,3],[1,3],[1,2]]))
+
+def create_adj_matrix(edges, amount_of_vertices):
+
+    graph = [[None for _ in range(amount_of_vertices)] for _ in range(amount_of_vertices)]
+
+    for u,v in edges:
+        graph[u][v] = True
+        graph[v][u] = True
+    return graph
+
+print(create_adj_list([(0,1),(1,2),(0,2)],3))
+print(create_adj_matrix([(0,1),(1,2),(0,2)],3))
