427a4b884ba25c96e4c73621b27c995deb7c7a7d
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2020-12-28 18:29:34 +0500

    neighbours of a vip node added

--- graphs/adj_matrix_to_list.py
+++ graphs/adj_matrix_to_list.py
@@ -4,8 +4,16 @@ def create_adj_list(adjacency_matrix):
 
     for row in range(n):
         for col in range(n):
-            if adjacency_matrix[row, col] == True:
+            if adjacency_matrix[row][col] == True:
                 graph[row].append(col)
     return graph
 
+def neighbours_of_vip_nodes(vip_nodes, graph):
     
+    neighbours_list = [[] for _ in range(len(graph))]
+
+    for node in vip_nodes:
+        for neighbor in graph[node]:
+            neighbours_list[neighbor] = True
+
+    return [node for node in neighbours_list if neighbours_list[node]]
