0627d37c4088fcbb281fbdd4d9bb643150e1c901
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2021-01-01 15:44:53 +0500

    is_connected graph added

--- graphs/is_graph_connected.py
+++ graphs/is_graph_connected.py
@@ -1,2 +1,12 @@
 def is_connected(graph):
-    
+    n = len(graph)
+    visited = [False for _ in range(n)]
+
+    def visit(node):
+        for neighbor in graph[node]:
+            if not visited[neighbor]:
+                visited[neighbor] = True
+                visit(neighbor)
+
+    visit(0)
+    return all(visited)
