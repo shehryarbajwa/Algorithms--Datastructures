f5913f15b3225ab44f99ee7ca547aa819239c923
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2021-01-01 15:51:09 +0500

    is_connected added

--- graphs/is_graph_connected.py
+++ graphs/is_graph_connected.py
@@ -1,3 +1,5 @@
+import unittest
+
 def is_connected(graph):
     n = len(graph)
     visited = [False for _ in range(n)]
@@ -10,3 +12,15 @@ def is_connected(graph):
 
     visit(0)
     return all(visited)
+
+
+class Test(unittest.TestCase):
+    def test_case_1(self):
+        self.assertEqual(is_connected([[1,2],[0,2],[0,1]]), True)
+
+    def test_case_2(self):
+        self.assertEqual(is_connected([[1],[2],[3],[],[5]]), False)
+
+
+if __name__ == "__main__":
+    unittest.main()
