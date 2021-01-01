748861cf70afe45dd978ad73940ddd6379580318
Shehryar Bajwa <shehryar.bajwa@gmail.com>
2021-01-01 17:04:12 +0500

    add connected components

--- graphs/is_graph_strongly_connected.py
+++ graphs/is_graph_strongly_connected.py
@@ -3,14 +3,27 @@ def is_strongly_connected(G, source):
     visited = [False for _ in range(n)]
 
 
-    def visit(node):
+    def visit(G, node):
         for neighbor in G[node]:
             if not visited[neighbor]:
                 visited[neighbor] = True
-                visit(neighbor)
+                visit(G, neighbor)
 
-    visit(source)
+    visit(G, source)
     if not all(visited):
         return False
+
+    G_reverse = [[] for _ in range(n)]
+    visited = [False for _ in range(n)]
+
+    for v in range(n):
+        for neighbor in G[v]:
+            G_reverse[neighbor].append(v)
+
+    visited[0] = True
+
+    visit(G_reverse, source)
+    return all(visited)
+
 
     
